123c9e126d0a1aae1c283e65c377fb8e5c935aff
calvin-mcelvain <mcelvain1@hotmail.com>
2025-02-06 12:27:08 -0600

    :recycle: refactor(src.output_manager): Making test_runs a list to handle replications.

--- src/output_manager.py
+++ src/output_manager.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass, field
 from typing import Dict, List
 from collections import defaultdict
 from models.base_model import RequestOut
+from test_config import TestConfig
 
 
 @dataclass
@@ -19,30 +20,56 @@ class StageRun:
 @dataclass
 class TestRun:
     stage_runs: Dict[str, StageRun]
-    test_id: str
+    test_config: TestConfig
     
     def get(self, stage: str, instance_type: str | None = None):
-        if instance_type:
-            return self.stage_runs[stage].get(instance_type)
-        else:
+        if stage in self.stage_runs:
+            if instance_type:
+                return self.stage_runs[stage].get(instance_type)
             return self.stage_runs[stage].stage_outputs
-    
+        return None
+
     def store(self, stage: str, output: RequestOut, instance_type: str):
+        if stage not in self.stage_runs:
+            self.stage_runs[stage] = StageRun(stage=stage)
         self.stage_runs[stage].store(instance_type, output)
     
 
 @dataclass
 class OutputManager:
-    test_runs: Dict[str, TestRun] = field(default_factory=dict)
+    test_runs: Dict[str, List[TestRun]] = field(default_factory=dict)
+    test_configs: Dict[str, TestConfig] = field(default_factory=dict)
     
     def get(self, test_id: str, stage: str, instance_type: str = None):
         if test_id in self.test_runs:
-            return self.test_runs[test_id].get(stage, instance_type)
+            for test_run in self.test_runs[test_id]:
+                result = test_run.get(stage, instance_type)
+                if result:
+                    return result
         return None
     
-    def store(self, test_id: str, stage: str, output: RequestOut, instance_type: str):
+    def store(
+        self, test_id: str, stage: str, output: RequestOut, instance_type: str
+    ):
         if test_id not in self.test_runs:
-            self.test_runs[test_id] = TestRun(stage_runs={}, test_id=test_id)
-        if stage not in self.test_runs[test_id].stage_runs:
-            self.test_runs[test_id].stage_runs[stage] = StageRun(stage=stage)
-        self.test_runs[test_id].store(stage, output, instance_type)
+            self.test_runs[test_id] = []
+        
+        if not any(stage in test_run.stage_runs for test_run in self.test_runs[test_id]):
+            test_config = self.test_configs.get(test_id)
+            new_test_run = TestRun(
+                test_id=test_id,
+                stage_runs={stage: StageRun(stage=stage)},
+                test_config=test_config
+            )
+            self.test_runs[test_id].append(new_test_run)
+
+        for test_run in self.test_runs[test_id]:
+            if stage in test_run.stage_runs:
+                test_run.store(stage, output, instance_type)
+                break
+    
+    def get_config(self, test_id: str):
+        return self.test_configs.get(test_id, None)
+
+    def add_config(self, test_config: TestConfig):
+        self.test_configs[test_config.id] = test_config
