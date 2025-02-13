5ad73cb071904854f8309d4ed2063af20e8e45f4
calvin-mcelvain <mcelvain1@hotmail.com>
2025-02-13 09:41:35 -0600

    :recycle: reactor(src.logger): Updating where outputs get logged
    All debugs get sent to a debug log file. And all info logs get sent to console and app file.

--- src/logger.py
+++ src/logger.py
@@ -1,53 +1,61 @@
 import sys
 import logging
 import logging.config
+from datetime import datetime
 from pathlib import Path
 from logging.handlers import RotatingFileHandler
 
-logs_path = Path(__file__).resolve().parents[1] / "logs"
+logs_path = Path("src").resolve().parents[1] / "logs"
 logs_path.mkdir(exist_ok=True, parents=True)
 
 
 def setup_logger():
-    app_log_file = logs_path / "app.log"
-
-    open(app_log_file, 'w').close()
+    today = datetime.now()
+    app_log_file = logs_path / "app-{}.log".format(today.strftime("%Y_%m_%d"))
+    debug_log_file = logs_path / "debug.log"
 
     logging.config.dictConfig({
         "version": 1,
         "disable_existing_loggers": False,
         "formatters": {
             "detailed": {
-                "format": "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+                "format": "%(asctime)s | %(levelname)s | %(name)s | %(message)s"
             }
         },
         "handlers": {
             "console": {
                 "class": "logging.StreamHandler",
-                "level": "DEBUG",
+                "level": "INFO",
                 "formatter": "detailed",
                 "stream": sys.stdout
             },
             "app_file": {
-                "class": "logging.handlers.RotatingFileHandler",
+                "class": "logging.FileHandler",
                 "filename": str(app_log_file),
                 "level": "INFO",
                 "formatter": "detailed",
                 "encoding": "utf-8",
-                "maxBytes": 5 * 1024 * 1024,
-                "backupCount": 5
+            },
+            "debug_file": {
+                "class": "logging.handlers.RotatingFileHandler",
+                "filename": str(debug_log_file),
+                "level": "DEBUG",
+                "formatter": "detailed",
+                "encoding": "utf-8",
+                "maxBytes": 5 * 1024 * 1024,  # 5 MB
+                "backupCount": 1,
             }
         },
         "loggers": {
             "app": {
-                "level": "INFO",
-                "handlers": ["app_file"],
+                "level": "DEBUG",
+                "handlers": ["console", "app_file", "debug_file"],
                 "propagate": False
             },
         },
         "root": {
             "level": "DEBUG",
-            "handlers": ["console", "app_file"]
+            "handlers": ["console", "app_file", "debug_file"]
         }
     })
 
