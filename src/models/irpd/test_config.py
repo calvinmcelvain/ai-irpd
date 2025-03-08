e3a0b2848473db482b54cd222a613385e8163468
calvin-mcelvain <mcelvain1@hotmail.com>
2025-03-08 11:11:10 -0600

    refactor: Adding typing classes to TestConfig model

--- src/models/irpd/test_config.py
+++ src/models/irpd/test_config.py
@@ -1,19 +1,22 @@
 from dataclasses import dataclass
+from typing import List, Optional
 from pathlib import Path
 from uuid import uuid4
+
+
 
 @dataclass
 class TestConfig:
     case: str
     ra: str
     treatment: str
-    llms: list[str]
+    llms: List[str]
     llm_config: str
     test_type: str
     test_path: Path
-    stages: list[str]
-    max_instances: int | None
-    id: str = None
+    stages: List[str]
+    max_instances: Optional[int]
+    id: Optional[str]
     
     def __post_init__(self):
         self.id = uuid4().hex
