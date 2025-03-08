bc5692d410dd9f3706ec62eb2c9643de6008502c
calvin-mcelvain <mcelvain1@hotmail.com>
2025-03-08 09:32:10 -0600

    refactor: ModelClass name change and absolute model path change
    ModelClass -> LLMModelClass

--- src/models/llm_model.py
+++ src/models/llm_model.py
@@ -10,7 +10,7 @@ CONFIGS = load_config("llm_configs.yml")
 
 
 @dataclass(frozen=True)
-class ModelClassContainer:
+class LLMModelClassContainer:
     module: str
     model_class: str
     model_config: str
@@ -22,13 +22,13 @@ class ModelClassContainer:
         return model_class, model_configs
 
 
-class ModelClass(ModelClassContainer, Enum):
-    GPT = ("llms.gpt", "GPT", "GPTConfigs")
-    CLAUDE = ("llms.claude", "Claude", "ClaudeConfigs")
-    GEMINI = ("llms.gemini", "Gemini", "GeminiConfigs")
-    NOVA = ("llms.nova", "Nova", "NovaConfigs")
-    MISTRAL = ("llms.mistral", "Mistral", "MistralConfigs")
-    GROK = ("llms.grok", "Grok", "GrokConfigs")
+class LLMModelClass(LLMModelClassContainer, Enum):
+    GPT = ("models.llms.gpt", "GPT", "GPTConfigs")
+    CLAUDE = ("models.llms.claude", "Claude", "ClaudeConfigs")
+    GEMINI = ("models.llms.gemini", "Gemini", "GeminiConfigs")
+    NOVA = ("models.llms.nova", "Nova", "NovaConfigs")
+    MISTRAL = ("models.llms.mistral", "Mistral", "MistralConfigs")
+    GROK = ("models.llms.grok", "Grok", "GrokConfigs")
 
 
 @dataclass(frozen=True)
@@ -42,7 +42,7 @@ class LLMModelContainer:
     key: str
     model_name: str
     api_key: str
-    model_class: ModelClass
+    model_class: LLMModelClass
     other_args: OtherArgs = field(default=OtherArgs())
 
 
@@ -51,90 +51,90 @@ class LLMModel(LLMModelContainer, Enum):
         "GPT_4O_0806",
         "gpt-4o-2024-08-06",
         "OPENAI_API_KEY",
-        ModelClass.GPT
+        LLMModelClass.GPT
     )
     GPT_4O_1120 = (
         "GPT_4O_1120",
         "gpt-4o-2024-11-20",
         "OPENAI_API_KEY",
-        ModelClass.GPT
+        LLMModelClass.GPT
     )
     GPT_4O_MINI_0718 = (
         "GPT_4O_MINI_0718",
         "gpt-4o-mini-2024-07-18",
         "OPENAI_API_KEY",
-        ModelClass.GPT,
+        LLMModelClass.GPT,
         OtherArgs(json_tool=True)
     )
     GPT_O1_1217 = (
         "GPT_O1_1217",
         "o1-2024-12-17",
         "OPENAI_API_KEY",
-        ModelClass.GPT
+        LLMModelClass.GPT
     )
     GPT_O1_MINI_0912 = (
         "GPT_O1_MINI_0912",
         "o1-mini-2024-09-12",
         "OPENAI_API_KEY",
-        ModelClass.GPT,
+        LLMModelClass.GPT,
         OtherArgs(json_tool=True)
     )
     GPT_O3_MINI_0131 = (
         "GPT_O3_MINI_0131",
         "o3-mini-2025-01-31",
         "OPENAI_API_KEY",
-        ModelClass.GPT
+        LLMModelClass.GPT
     )
     GROK_2_1212 = (
         "GROK_2_1212",
         "grok-2-1212",
         "XAI_API_KEY",
-        ModelClass.GROK
+        LLMModelClass.GROK
     )
     CLAUDE_3_5_SONNET = (
         "CLAUDE_3_5_SONNET",
         "claude-3.5-sonnet-20241022",
         "ANTHROPIC_API_KEY",
-        ModelClass.CLAUDE,
+        LLMModelClass.CLAUDE,
         OtherArgs(json_tool=True)
     )
     CLAUDE_3_7_SONNET = (
         "CLAUDE_3_7_SONNET",
         "claude-3-7-sonnet-20250219",
         "ANTHROPIC_API_KEY",
-        ModelClass.CLAUDE,
+        LLMModelClass.CLAUDE,
         OtherArgs(json_tool=True)
     )
     GEMINI_2_FLASH = (
         "GEMINI_2_FLASH",
         "gemini-2.0-flash-001",
         "GOOGLE_API_KEY",
-        ModelClass.GEMINI
+        LLMModelClass.GEMINI
     )
     GEMINI_2_FLASH_LITE = (
         "GEMINI_2_FLASH_LITE",
         "gemini-2.0-flash-lite-001",
         "GOOGLE_API_KEY",
-        ModelClass.GEMINI
+        LLMModelClass.GEMINI
     )
     GEMINI_1_5_PRO = (
         "GEMINI_1_5_PRO",
         "gemini-1.5-pro-002",
         "GOOGLE_API_KEY",
-        ModelClass.GEMINI
+        LLMModelClass.GEMINI
     )
     NOVA_PRO_V1 = (
         "NOVA_PRO_V1",
         "amazon.nova-pro-v1:0",
         "BEDROCK_API_KEY",
-        ModelClass.NOVA,
+        LLMModelClass.NOVA,
         OtherArgs(json_tool=True, region="us-east-1")
     )
     MISTRAL_LARGE_2411 = (
         "MISTRAL_LARGE_2411",
         "mistral-large-2411",
         "MISTRAL_API_KEY",
-        ModelClass.MISTRAL
+        LLMModelClass.MISTRAL
     )
     
     def get_model_instance(
