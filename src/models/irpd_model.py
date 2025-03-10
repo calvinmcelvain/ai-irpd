983915fc7678f54215189c50b480153f744cdfbe
calvin-mcelvain <mcelvain1@hotmail.com>
2025-03-10 11:33:49 -0500

    refactor: Adjusting module call, typing validations, and model name for IRPDTestClass model

--- src/models/irpd_model.py
+++ src/models/irpd_model.py
@@ -1,11 +1,14 @@
 import logging
-from typing import List
+from typing import List, Union
 from dataclasses import dataclass
 from enum import Enum
 from functools import cached_property
+
 from utils import lazy_import
 
+
 log = logging.getLogger(__name__)
+
 
 
 @dataclass(frozen=True)
@@ -18,19 +21,19 @@ class TestClassContainer:
         return lazy_import(self.module, self.test_class)
     
 
-class TestClass(TestClassContainer, Enum):
-    TEST = ("irpd.test", "Test")
-    SUBTEST = ("irpd.subtest", "Subtest")
-    CROSS_MODEL = ("irpd.cross_model", "CrossModel")
-    INTRA_MODEL = ("irpd.intra_model", "IntraModel")
-    SAMPLE_SPLITTING = ("irpd.sample_splitting", "SampleSplitting")
+class IRPDTestClass(TestClassContainer, Enum):
+    TEST = ("models.irpd.test", "Test")
+    SUBTEST = ("models.irpd.subtest", "Subtest")
+    CROSS_MODEL = ("models.irpd.cross_model", "CrossModel")
+    INTRA_MODEL = ("models.irpd.intra_model", "IntraModel")
+    SAMPLE_SPLITTING = ("models.irpd.sample_splitting", "SampleSplitting")
     
     def get_type_instance(
         self,
-        case: str,
-        ras: List[str] | str,
-        treatments: List[str] | str,
-        stages: List[str],
+        case: Union[List[str], str],
+        ras: Union[List[str], str],
+        treatments: Union[List[str], str],
+        stages: Union[List[str], str],
         **kwargs
     ):
         test_class = self.impl
