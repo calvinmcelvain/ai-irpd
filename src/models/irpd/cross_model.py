bf19370029f891704f1d9c90a65349a748364735
calvin-mcelvain <mcelvain1@hotmail.com>
2025-03-30 08:25:05 -0500

    feat: Adding batch arg to CrossModel model

--- src/models/irpd/cross_model.py
+++ src/models/irpd/cross_model.py
@@ -80,6 +80,7 @@ class CrossModel(IRPDBase):
         self,
         max_instances: Optional[int] = None,
         config_ids: Union[str, List[str]] = None,
+        batch: bool = False,
         print_response: bool = False
     ):
         test = self.test_type.upper()
