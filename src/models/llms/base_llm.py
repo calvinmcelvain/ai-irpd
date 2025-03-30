91e2c4f583f872aac066dd8ef23561ab2cdc469a
calvin-mcelvain <mcelvain1@hotmail.com>
2025-03-30 11:43:40 -0500

    refactor: batch_file arg in batch request method now batch arg thats wither str (file path) or list of requests

--- src/models/llms/base_llm.py
+++ src/models/llms/base_llm.py
@@ -1,5 +1,5 @@
 from pydantic import BaseModel
-from typing import List
+from typing import List, Union
 from abc import ABC, abstractmethod
 
 from models.prompts import Prompts
@@ -75,7 +75,7 @@ class BaseLLM(ABC):
         pass
     
     @abstractmethod
-    def batch_request(self, batch_file: str):
+    def batch_request(self, batch: Union[str, List]):
         pass
     
     @abstractmethod
