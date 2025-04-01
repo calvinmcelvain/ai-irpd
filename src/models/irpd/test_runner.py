708afcfdf2fe0a66c4cacb22f26578c8523f7bae
calvin-mcelvain <mcelvain1@hotmail.com>
2025-04-01 12:59:38 -0500

    feat: Initializing TestRunner functional model

---
+++ src/models/irpd/test_runner.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+
+from models.irpd.test_config import TestConfig
+
+
+
+class TestRunner:
+    def __init__(
+        self,
+        config: TestConfig,
+        sub_path: Path
+    ):
+        pass
