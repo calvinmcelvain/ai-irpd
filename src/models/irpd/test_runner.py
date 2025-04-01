b36c2bad3915d10a2ef8aad27a7b4bf254e9d37d
calvin-mcelvain <mcelvain1@hotmail.com>
2025-04-01 18:47:08 -0500

    feat: Adding initialize llm to TestRunner functional model

--- src/models/irpd/test_runner.py
+++ src/models/irpd/test_runner.py
@@ -1,6 +1,11 @@
+import logging
 from pathlib import Path
 
 from models.irpd.test_config import TestConfig
+from models.llm_model import LLMModel
+
+
+log = logging.getLogger(__name__)
 
 
 
@@ -8,6 +13,23 @@ class TestRunner:
     def __init__(
         self,
         config: TestConfig,
-        sub_path: Path
+        llm: str,
+        sub_path: Path,
+        print_response: bool = False
     ):
+        self.stages = config.stages
+        self.batch_request = config.batches
+        self.llm_config = config.llm_config
+        
+        self.llm = self._generate_llm_instance(llm)
+        self.print_response = print_response
+        self.sub_path = sub_path
+        
+    
+    def _generate_llm_instance(self, llm: str):
+        return getattr(LLMModel, llm).get_llm_instance(
+            self.llm_config, self.print_response
+        )
+    
+    def run(self):
         pass
