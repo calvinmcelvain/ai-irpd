1c73f0b8a3baeb955bccc2e55d752ea5f12246e7
calvin-mcelvain <mcelvain1@hotmail.com>
2025-04-09 10:10:28 -0500

    refacor & docs: Refactoring (just moving models around) and adding docustrings/comments for schemas module

--- src/models/irpd/schemas.py
+++ src/models/irpd/schemas.py
@@ -1,12 +1,14 @@
+"""
+Schemas module.
+
+Contains the Pydantic output schemas for all stages. Used for JSON structured 
+outputs of LLMs.
+"""
 from pydantic import BaseModel
 
 
-
-class Stage0Schema(BaseModel):
-    window_number: int
-    summary: str
-
-
+# Schemas for stage schemas containing list of outputs (such as categories or
+# category assignments/classifications)
 class Examples(BaseModel):
     window_number: int
     reasoning: str
@@ -16,6 +18,21 @@ class Category(BaseModel):
     category_name: str
     definition: str
     examples: list[Examples]
+
+
+class CategoryAssignment(BaseModel):
+    category_name: str
+
+
+class Ranking(BaseModel):
+    category_name: str
+    rank: int
+
+
+# Stage schemas
+class Stage0Schema(BaseModel):
+    window_number: int
+    summary: str
 
 
 class Stage1Schema(BaseModel):
@@ -30,19 +47,10 @@ class Stage1cSchema(BaseModel):
     refined_categories: list[Category]
 
 
-class CategoryAssignment(BaseModel):
-    category_name: str
-
-
 class Stage2Schema(BaseModel):
     window_number: int
     assigned_categories: list[CategoryAssignment]
     reasoning: str
-
-
-class Ranking(BaseModel):
-    category_name: str
-    rank: int
 
 
 class Stage3Schema(BaseModel):
