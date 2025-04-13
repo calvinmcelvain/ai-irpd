9c58e1b7e1902e5a931ca16b05f6acfce8d6f071
calvin-mcelvain <mcelvain1@hotmail.com>
2025-04-12 20:30:59 -0500

    refactor: Removing prompts field since already in REquestOut model

--- src/types/irpd_request.py
+++ src/types/irpd_request.py
@@ -4,7 +4,6 @@ Contains IRPDRequest model.
 from dataclasses import dataclass
 from pathlib import Path
 
-from types.prompts import Prompts
 from types.request_output import RequestOut
 
 
@@ -12,7 +11,6 @@ from types.request_output import RequestOut
 @dataclass
 class IRPDRequest:
     output: RequestOut
-    prompts: Prompts = None
     prompt_id: str = None
     prompt_path: Path = None
     response_path: Path = None
