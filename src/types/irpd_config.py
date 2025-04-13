e36af1d28be82d998969023638b4910c6ae025d9
calvin-mcelvain <mcelvain1@hotmail.com>
2025-04-13 12:49:02 -0500

    refactor: IRPDConfig no longer has Path objects

--- src/types/irpd_config.py
+++ src/types/irpd_config.py
@@ -3,46 +3,31 @@ IRPD config module.
 
 Contains the IRPDConfig model & it's dataclass methods.
 """
-from dataclasses import dataclass, asdict
+from dataclasses import dataclass
 from typing import List, Optional
-from pathlib import Path
 from uuid import uuid4
 
 
 
 @dataclass
 class IRPDConfig:
-    case: str = None
-    ra: str = None
-    treatment: str = None
-    llms: List[str] = None
-    llm_config: str = None
-    test_type: str = None
-    test_path: Path = None
-    data_path: Path = None
-    prompts_path: Path = None
-    stages: List[str] = None
-    batches: bool = None
-    total_replications: int = None
+    case: str
+    ra: str
+    treatment: str
+    llms: List[str]
+    llm_config: str
+    test_type: str
+    test_path: str
+    data_path: str
+    prompts_path: str
+    stages: List[str]
+    batches: bool
+    total_replications: int
+    max_instances: Optional[int]
     cases: List[str] = None
-    max_instances: Optional[int] = None
-    id: str = None
+    id: str = uuid4().hex
     
     def __post_init__(self):
-        # A config ID is a UUID
-        self.id = uuid4().hex
-        
         # A case can be a composition of cases, defined with a `_` (e.g., 
         # 'uni_switch'). This is accounted for via a `cases` field.
         self.cases = self.case.split("_")
-    
-    def convert_to_dict(self):
-        """
-        Converts all fields that are Path objects into a string.
-        
-        Used for writing meta data, as problems arrise when tryint to write 
-        Path objects in JSON.
-        """
-        return asdict(self, dict_factory=lambda x: {
-            k: (v.as_posix() if isinstance(v, Path) else v) for k, v in x
-        })
