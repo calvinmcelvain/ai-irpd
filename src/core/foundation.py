4300b7f023e8a1b47c9430ea27b6349046ad179c
calvin-mcelvain <mcelvain1@hotmail.com>
2025-04-14 15:22:55 -0500

    build: Adding a FoundationalModel for inheritance of functional models

---
+++ src/core/foundation.py
@@ -0,0 +1,80 @@
+"""
+Contains the FoundationalModel
+"""
+from abc import ABC
+from pathlib import Path
+
+from helpers.utils import create_directory, lazy_import
+from core.functions import instance_types
+from core.llms.llm_models import LLMModel
+from _types.irpd_config import IRPDConfig
+
+
+
+class FoundationalModel(ABC):
+    def __init__(self, irpd_config: IRPDConfig):
+        self.print_response = False # TestRunner sets this again.
+        self.irpd_config = irpd_config
+        self.stages = irpd_config.stages
+        self.cases = irpd_config.cases
+        self.batches = irpd_config.batches
+        self.test_path = Path(irpd_config.test_path)
+        self.data_path = Path(irpd_config.data_path)
+        self.prompts_path = Path(irpd_config.prompts_path)
+        self.max_instances = irpd_config.max_instances
+        self.llms = irpd_config.llms
+        self.case = irpd_config.case
+        self.ra = irpd_config.ra
+        self.treatment = irpd_config.treatment
+        self.llm_config = irpd_config.llm_config
+        self.total_replications = irpd_config.total_replications
+        self.schemas = {
+            stage: lazy_import("types.irpd_stage_schemas", f"Stage{stage}Schema")
+            for stage in self.stages
+        }
+        self.subsets = {
+            stage: self._get_subsets(stage)
+            for stage in self.stages
+        }
+        
+    def _generate_subpath(self, n: int, llm_str: str):
+        """
+        Generates a subpath for a given replication and LLM. 
+        
+        For tests w/ more than one LLM, a dir. is made for each LLM. For tests 
+        w/ more than one replication, a dir. is made for each replicaiton (w/ 
+        respect to the LLM dir.)
+        """
+        subpath = self.test_path
+        if len(self.llms) > 1: subpath = subpath / llm_str
+        if self.total_replications > 1: subpath = subpath / f"replication_{n}"
+        if not subpath.exists(): create_directory(subpath)
+        return subpath
+    
+    def _get_subsets(self, stage_name: str):
+        """
+        Generates subsets for a given stage.
+        """
+        subsets = ["full"]
+        if stage_name in {"1", "1r"}:
+            prod = [
+                (case, instance_type)
+                for case in self.cases
+                for instance_type in instance_types(case)
+            ]
+            subsets += [f"{c}_{i}" for c, i in prod]
+        return subsets
+    
+    def _generate_meta_path(self, n: int, llm_str: str):
+        """
+        Generates the path for 'test' meta. File exists for each subpath.
+        """
+        subpath = self._generate_subpath(n, llm_str)
+        return subpath / "_test_meta.json"
+    
+    def _generate_llm_instance(self, llm_str: str):
+            """
+            Returns the LLM model instance from the /llms package.
+            """
+            return getattr(LLMModel, llm_str).get_llm_instance(
+                self.llm_config, self.print_reponse)
