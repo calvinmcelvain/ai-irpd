fb763f09a77bf78b77c38bef0c96b2d3914251de
calvin-mcelvain <mcelvain1@hotmail.com>
2025-04-14 14:34:16 -0500

    refactor: Refactoring StageOutput object

--- src/_types/stage_output.py
+++ src/_types/stage_output.py
@@ -1,21 +1,12 @@
-"""
-Stage output model.
-"""
 from pathlib import Path
-from typing import List
-from dataclasses import dataclass, field
+from typing import List, Dict
+from dataclasses import dataclass
 
-from _types.request_output import RequestOut
+from _types.irpd_output import IRPDOutput
 
 
 @dataclass
 class StageOutput:
     stage_name: str
-    subset: str
-    llm_str: str
-    replication: int
-    output_path: Path
-    batch_id: str = None
-    batch_path: Path = None
-    outputs: List[RequestOut] = field(default_factory=list)
-    complete: bool = False
+    stage_path: Path
+    outputs: Dict[str, List[IRPDOutput]]
