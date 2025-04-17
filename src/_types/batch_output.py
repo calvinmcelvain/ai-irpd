55cfb3f798ea128cf6176dcaad534efb0e2f6835
calvin-mcelvain <mcelvain1@hotmail.com>
2025-04-17 17:19:27 -0500

    refactor: BatchOut object now IRPDOutput

--- src/_types/batch_output.py
+++ src/_types/batch_output.py
@@ -1,14 +1,14 @@
 from typing import List
 from dataclasses import dataclass
 
-from _types.request_output import RequestOut
+from _types.irpd_output import IRPDOutput
 
 
 
 @dataclass
 class BatchResponse:
     response_id: str
-    response: RequestOut
+    response: IRPDOutput
 
 
 @dataclass
