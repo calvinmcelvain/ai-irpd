c13f5fa0f28e0d8f59fbb99af2a4fd70f3375853
calvin-mcelvain <mcelvain1@hotmail.com>
2025-04-19 10:07:55 -0500

    refactor: Defining types

--- src/core/prompt_composers/prompt_composer.py
+++ src/core/prompt_composers/prompt_composer.py
@@ -4,11 +4,16 @@ Contains the PromptComposer model.
 Aggregates all PromptComposer models.
 """
 from dataclasses import dataclass
+from typing import TypeVar, Type
 from enum import Enum
 from functools import cached_property
 
 from helpers.utils import dynamic_import
+from core.prompt_composers.base_composer import BaseComposer
 from _types.irpd_config import IRPDConfig
+
+
+T = TypeVar("T", bound=BaseComposer)
 
 
 
@@ -19,18 +24,19 @@ class PromptComposerContainer:
     stage_name: str
     
     @cached_property
-    def impl(self):
-        return dynamic_import(self.module, self.test_class)
+    def impl(self) -> Type[T]:
+        model_class: Type[T] = dynamic_import(self.module, self.test_class)
+        return model_class
 
 
 
 class PromptComposer(PromptComposerContainer, Enum):
-    stage_0 = ("core.prompt_composers.stage_0", "Stage0PromptComposer", "0")
-    stage_1 = ("core.prompt_composers.stage_1", "Stage1PromptComposer", "1")
-    stage_1r = ("core.prompt_composers.stage_1r", "Stage1rPromptComposer", "1r")
-    stage_1c = ("core.prompt_composers.stage_1c", "Stage1cPromptComposer", "1c")
-    stage_2 = ("core.prompt_composers.stage_2", "Stage2PromptComposer", "2")
-    stage_3 = ("core.prompt_composers.stage_3", "Stage3PromptComposer", "3")
+    STAGE_0 = ("core.prompt_composers.stage_0", "Stage0PromptComposer", "0")
+    STAGE_1 = ("core.prompt_composers.stage_1", "Stage1PromptComposer", "1")
+    STAGE_1r = ("core.prompt_composers.stage_1r", "Stage1rPromptComposer", "1r")
+    STAGE_1c = ("core.prompt_composers.stage_1c", "Stage1cPromptComposer", "1c")
+    STAGE_2 = ("core.prompt_composers.stage_2", "Stage2PromptComposer", "2")
+    STAGE_3 = ("core.prompt_composers.stage_3", "Stage3PromptComposer", "3")
     
     def get_prompt_composer(self, irpd_config: IRPDConfig):
         """
